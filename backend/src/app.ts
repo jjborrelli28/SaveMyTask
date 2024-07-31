af800711083f725d0ae067a8fec05a0f0f024641
jjborrelli28 <jjborrelli.secondary@gmail.com>
2024-07-31 16:42:18 -0300

    config cors properties

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,13 @@ import router from "./routes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_HOST,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/api", router);
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import app from "./app";
 
 dotenv.config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
