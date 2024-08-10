2216069f195e2e3a7ff4c9f9b4c044748f2cbb73
jjborrelli28 <jjborrelli.secondary@gmail.com>
2024-08-10 18:00:27 -0300

    add pagination (unfinished)

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -3,20 +3,52 @@ import db from "../database/db";
 import { Task } from "../types";
 import { broadcast } from "../web-socket";
 
+const getUpdatedTaskList = async ({
+  search,
+  page,
+  limit,
+}: {
+  search: string;
+  page: number;
+  limit: number;
+}) => {
+  const offset = (page - 1) * limit;
+
+  const list = await db
+    .selectFrom("task")
+    .selectAll()
+    .where("description", "like", `%${search}%`)
+    .limit(limit)
+    .offset(offset)
+    .orderBy("created_at", "desc")
+    .execute();
+  return { list, total: list.length + 1 };
+};
+
 export const getTaskList = async (req: Request, res: Response) => {
-  const search = req.query?.search as string | undefined;
+  const {
+    page = "1",
+    limit = "20",
+    search = "",
+  } = req.query as { page: string; limit: string; search: string };
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
+
+  if (isNaN(pageNumber) || pageNumber <= 0) {
+    return res.status(400).json({ error: "Invalid page number" });
+  }
+  if (isNaN(limitNumber) || limitNumber <= 0) {
+    return res.status(400).json({ error: "Invalid limit number" });
+  }
 
   try {
-    let taskList = await db.selectFrom("task").selectAll().execute();
+    const { list, total } = await getUpdatedTaskList({
+      search,
+      page: pageNumber,
+      limit: limitNumber,
+    });
 
-    if (search !== undefined) {
-      taskList = taskList.filter((task) =>
-        task.description.toLowerCase().includes(search.toLowerCase())
-      );
-      broadcast({ type: "TASK_LIST_UPDATE", taskList });
-    }
-
-    res.json(taskList);
+    res.json({ list, total, page: pageNumber, limit: limitNumber });
   } catch (error) {
     res
       .status(500)
@@ -52,6 +84,13 @@ export const getTaskById = async (req: Request, res: Response) => {
 
 export const createTask = async (req: Request, res: Response) => {
   const { description, state, user_id } = req.body as Task;
+  const {
+    page = "1",
+    limit = "20",
+    search = "",
+  } = req.query as { page: string; limit: string; search: string };
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
 
   if (!description || !state || !user_id) {
     return res
@@ -69,8 +108,13 @@ export const createTask = async (req: Request, res: Response) => {
       })
       .executeTakeFirstOrThrow();
 
-    const taskList = await db.selectFrom("task").selectAll().execute();
-    broadcast({ type: "TASK_LIST_UPDATE", taskList });
+    const { list } = await getUpdatedTaskList({
+      search,
+      page: pageNumber,
+      limit: limitNumber,
+    });
+
+    broadcast({ type: "UPDATE_TASK_LIST", list });
 
     const newTask = await db
       .selectFrom("task")
@@ -88,6 +132,13 @@ export const createTask = async (req: Request, res: Response) => {
 
 export const updateTask = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
+  const {
+    page = "1",
+    limit = "20",
+    search = "",
+  } = req.query as { page: string; limit: string; search: string };
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
 
   if (isNaN(id)) {
     return res.status(400).json({ error: "Invalid task id" });
@@ -114,8 +165,13 @@ export const updateTask = async (req: Request, res: Response) => {
       return res.status(404).json({ error: "Task not found" });
     }
 
-    const taskList = await db.selectFrom("task").selectAll().execute();
-    broadcast({ type: "TASK_LIST_UPDATE", taskList });
+    const { list } = await getUpdatedTaskList({
+      search,
+      page: pageNumber,
+      limit: limitNumber,
+    });
+
+    broadcast({ type: "UPDATE_TASK_LIST", list });
 
     const updatedTask = await db
       .selectFrom("task")
@@ -134,6 +190,13 @@ export const updateTask = async (req: Request, res: Response) => {
 
 export const deleteTask = async (req: Request, res: Response) => {
   const taskId = parseInt(req.params.id, 10);
+  const {
+    page = "1",
+    limit = "20",
+    search = "",
+  } = req.query as { page: string; limit: string; search: string };
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
 
   if (isNaN(taskId)) {
     return res.status(400).json({ error: "Invalid task id" });
@@ -149,8 +212,13 @@ export const deleteTask = async (req: Request, res: Response) => {
       return res.status(404).json({ error: "Task not found" });
     }
 
-    const taskList = await db.selectFrom("task").selectAll().execute();
-    broadcast({ type: "TASK_LIST_UPDATE", taskList });
+    const { list } = await getUpdatedTaskList({
+      search,
+      page: pageNumber,
+      limit: limitNumber,
+    });
+
+    broadcast({ type: "UPDATE_TASK_LIST", list });
 
     res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
